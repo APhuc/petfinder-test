3bada6d2a3999398115a6f8fd370f00eaad56686
Phuc Nguyen <p30012k1@gmail.com>
2023-02-27 20:31:46 +0700

    up

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -6,21 +6,23 @@ import Header from "../../Components/Header";
 import "../../Styles/Pages/Home/style.css";
 import Data from "../../Model/animals.json";
 import CardItem from "../../Components/CardItem";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function HomePage() {
   const { access_token, token_type } = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const [state,setState] = useState({
-    animals:[],
-    pagination: {}
-  })
+  // const [state, setState] = useState({
+  //   animals: [],
+  //   pagination: {},
+  // });
+  const [state, setState] = useState(Data);
+  const [hasMore, setHasMore] = useState(true);
 
-  console.log(Data);
   // const getProduct = async () => {
   //   const TOKEN = `${token_type} ${access_token}`;
   //   const config = {
   //     headers: { Authorization: `${TOKEN}`, "Content-Type": "application/json" },
-  //     params: { limit: 5, page: 2 },
+  //     params: { limit: 60, page: 1 },
   //   };
   //   const post = await productApi.getAll(config);
   //   console.log(post, "Postssss");
@@ -29,27 +31,40 @@ export default function HomePage() {
   // useLayoutEffect(() => {
   //   getProduct();
   // }, []);
+  const fetchMoreData = () => {
+    if (state.animals.length < state.pagination.total_count) {
+      setState((prev) => ({ ...prev, animals: prev.animals.concat(Data.animals), pagination: Data.pagination }));
+    } else {
+      setHasMore(false);
+    }
+  };
   return (
     <div className="home-page">
       <Header />
-
       <div className="container">
-        <div className="gallery">
-          {Data.animals?.map((item, index) => {
-            return (
-              <div key={index}>
-                <CardItem
-                  url={item?.photos[0]?.full}
-                  breeds={item?.breeds}
-                  species={item?.species}
-                  name={item?.name}
-                  description={item?.description}
-                  age={item.age}
-                />
-              </div>
-            );
-          })}
-        </div>
+        <InfiniteScroll
+          next={fetchMoreData}
+          dataLength={state?.animals.length}
+          hasMore={hasMore}
+          loader={<h4 >Loading...</h4>}
+          scrollableTarget="scrollableDiv">
+          <div className="gallery">
+            {state.animals?.map((item, index) => {
+              return (
+                <div key={index}>
+                  <CardItem
+                    url={item?.photos[0]?.full}
+                    breeds={item?.breeds}
+                    species={item?.species}
+                    name={item?.name}
+                    description={item?.description}
+                    age={item.age}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </InfiniteScroll>
       </div>
     </div>
   );
